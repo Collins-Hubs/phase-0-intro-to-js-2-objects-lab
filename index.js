7836193cf23fad263e7475f42d9f059a4d0e6c1d
Collins <collinskibet7545@gmail.com>
2024-06-30 22:52:13 +0300

    Update

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+	name: 'Collins',
+	streetAddress: 'Mirema'
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+	return{
+		...employee,
+		[key]: value,
+	};
+}
+const updated = updateEmployeeWithKeyAndValue(employee, "name", "Mosee");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  return { ...employee[key] };
+}
+
+const newEmployee = deleteFromEmployeeByKey(employee, "name");
+delete newEmployee.name;
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
